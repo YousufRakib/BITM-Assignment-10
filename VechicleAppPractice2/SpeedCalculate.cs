6c15341f177f12d971379f51698bc859fe29a6f4
YousufRakib <rakibkhanphc@gmail.com>
2019-10-17 01:37:50 +0600

    Vehicle

---
+++ VechicleAppPractice2/SpeedCalculate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    class SpeedCalculate
+    {
+        public double maxspeed(List<double> speed)
+        {
+            double maxSpeed = 0;
+            for(int i = 0; i < speed.Count(); i++)
+            {
+                if (speed[i] > maxSpeed)
+                {
+                    maxSpeed = speed[i];
+                }
+            }
+            return maxSpeed;
+        }
+        public double minspeed(List<double> speed)
+        {
+            double minSpeed = double.MaxValue;
+            for (int i = 0; i < speed.Count(); i++)
+            {
+                if (speed[i] <=minSpeed)
+                {
+                    minSpeed =Convert.ToDouble(speed[i]);
+                }
+            }
+            return minSpeed;
+        }
+        public double avgspeed(List<double> speed)
+        {
+            double avgSpeed = 0;
+            double sum = 0;
+            for (int i = 0; i < speed.Count(); i++)
+            {
+                sum = sum + speed[i];
+            }
+            avgSpeed = sum / speed.Count();
+            return avgSpeed;
+        }
+
+    }
+}
---
+++ VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+        SpeedCalculate _speedCalculate = new SpeedCalculate();
+        string name;
+        string regNumber;
+        List<double> speed = new List<double> { };
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            name = vehicleNameTextBox.Text;
+            regNumber = regNoTextBox.Text;
+            MessageBox.Show("Saved");
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            speed.Add(Convert.ToDouble(speedTextBox.Text));
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(vehicleNameTextBox.Text))
+            {
+                MessageBox.Show("Please Enter vehicle name");
+                return;
+            }
+            if (!String.IsNullOrEmpty(regNoTextBox.Text))
+            {
+                MessageBox.Show("Please Enter registration number");
+                return;
+            }
+
+            //Max Speed Show
+            double max = _speedCalculate.maxspeed(speed);
+            maxSpeedTextBox.Text = max.ToString();
+
+            //Min Speed Show
+            double min = _speedCalculate.minspeed(speed);
+            minSpeedTextBox.Text = min.ToString();
+
+            //Avg Speed Show
+            double avg = _speedCalculate.avgspeed(speed);
+            averageSpeedTextBox.Text = avg.ToString();
+        }
+    }
+}
